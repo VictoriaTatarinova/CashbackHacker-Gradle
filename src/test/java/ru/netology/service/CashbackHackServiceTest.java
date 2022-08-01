c178c74415dded83c0fb574aa3922c7a2b49ef79
VictoriaTatarinova <chiarouomo@gmail.com>
2022-08-01 14:02:32 +0200

    Added Tests Junit4, Jupiter.api

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,69 +1,124 @@
 package ru.netology.service;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
-import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void ShouldBuy100() {
         int amount = 900;
         int expected = 100;
         int actual = service.remain(amount);
 
-        assertEquals(actual, expected);
+        org.junit.jupiter.api.Assertions.assertEquals(actual, expected);
         System.out.println("Для получения бонуса Вам нужно докупить на сумму " + expected + " рублей");
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void ShouldBuy10() {
         int amount = 990;
         int expected = 10;
         int actual = service.remain(amount);
 
-        assertEquals(actual, expected);
+        org.junit.jupiter.api.Assertions.assertEquals(actual, expected);
         System.out.println("Для получения бонуса Вам нужно докупить на сумму " + expected + " рублей");
     }
 
-//    @Test
-//    public void ShouldNotBuy1000() {
-//        int amount = 1_000;
-//        int expected = 0;
-//        int actual = service.remain(amount);
-//
-//        assertEquals(actual, expected);
-//        System.out.println("Для получения бонуса Вам нужно докупить на сумму " + expected + " рублей");
-//    }
+    @org.junit.jupiter.api.Test
+    public void ShouldNotBuy1000() {
+        int amount = 1_000;
+        int expected = 0;
+        int actual = service.remain(amount);
 
-//    @Test
-//    public void ShouldNotBuy5000() {
-//        int amount = 5_000;
-//        int expected = 0;
-//        int actual = service.remain(amount);
-//
-//        assertEquals(actual, expected);
-//        System.out.println("Для получения бонуса Вам нужно докупить на сумму " + expected + " рублей");
-//    }
+        org.junit.jupiter.api.Assertions.assertEquals(actual, expected);
+    }
 
-    @Test
+    @org.junit.jupiter.api.Test
+    public void ShouldNotBuy5000() {
+        int amount = 5_000;
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        org.junit.jupiter.api.Assertions.assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
     public void ShouldBuyForMoreBonus() {
         int amount = 2_500;
         int expected = 500;
         int actual = service.remain(amount);
 
-        assertEquals(actual, expected);
+        org.junit.jupiter.api.Assertions.assertEquals(actual, expected);
         System.out.println("Для получения бонуса Вам нужно докупить на сумму " + expected + " рублей");
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void ShouldBuyZero() {
         int amount = 0;
         int expected = 1_000;
         int actual = service.remain(amount);
 
-        assertEquals(actual, expected);
+        org.junit.jupiter.api.Assertions.assertEquals(actual, expected);
+        System.out.println("Для получения бонуса Вам нужно докупить на сумму " + expected + " рублей");
+    }
+
+    @org.junit.Test
+    public void Junit4ShouldBuy100() {
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+
+        org.junit.Assert.assertEquals(actual, expected);
+        System.out.println("Для получения бонуса Вам нужно докупить на сумму " + expected + " рублей");
+    }
+
+    @org.junit.Test
+    public void Junit4ShouldBuy10() {
+        int amount = 990;
+        int expected = 10;
+        int actual = service.remain(amount);
+
+        org.junit.Assert.assertEquals(actual, expected);
+        System.out.println("Для получения бонуса Вам нужно докупить на сумму " + expected + " рублей");
+    }
+
+    @org.junit.Test
+    public void Junit4ShouldNotBuy1000() {
+        int amount = 1_000;
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void Junit4ShouldNotBuy5000() {
+        int amount = 5_000;
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void Junit4ShouldBuyForMoreBonus() {
+        int amount = 2_500;
+        int expected = 500;
+        int actual = service.remain(amount);
+
+        org.junit.Assert.assertEquals(actual, expected);
+        System.out.println("Для получения бонуса Вам нужно докупить на сумму " + expected + " рублей");
+    }
+
+    @org.junit.Test
+    public void Junit4ShouldBuyZero() {
+        int amount = 0;
+        int expected = 1_000;
+        int actual = service.remain(amount);
+
+        org.junit.Assert.assertEquals(actual, expected);
         System.out.println("Для получения бонуса Вам нужно докупить на сумму " + expected + " рублей");
     }
 
