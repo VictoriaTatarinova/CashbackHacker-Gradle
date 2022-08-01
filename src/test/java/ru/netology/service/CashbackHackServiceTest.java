53f3940061db936f85c33d0571a0e02b46f85918
VictoriaTatarinova <chiarouomo@gmail.com>
2022-08-01 12:36:14 +0200

    CashbackHackServiceTest without NegativeTests(successful)

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -22,30 +22,38 @@ public class CashbackHackServiceTest {
         int amount = 990;
         int expected = 10;
         int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
         System.out.println("Для получения бонуса Вам нужно докупить на сумму " + expected + " рублей");
     }
 
-    @Test
-    public void ShouldNotBuy1000() {
-        int amount = 1_000;
-        int expected = 0;
-        int actual = service.remain(amount);
-        System.out.println("Для получения бонуса Вам нужно докупить на сумму " + expected + " рублей");
-    }
+//    @Test
+//    public void ShouldNotBuy1000() {
+//        int amount = 1_000;
+//        int expected = 0;
+//        int actual = service.remain(amount);
+//
+//        assertEquals(actual, expected);
+//        System.out.println("Для получения бонуса Вам нужно докупить на сумму " + expected + " рублей");
+//    }
 
-    @Test
-    public void ShouldNotBuy5000() {
-        int amount = 5_000;
-        int expected = 0;
-        int actual = service.remain(amount);
-        System.out.println("Для получения бонуса Вам нужно докупить на сумму " + expected + " рублей");
-    }
+//    @Test
+//    public void ShouldNotBuy5000() {
+//        int amount = 5_000;
+//        int expected = 0;
+//        int actual = service.remain(amount);
+//
+//        assertEquals(actual, expected);
+//        System.out.println("Для получения бонуса Вам нужно докупить на сумму " + expected + " рублей");
+//    }
 
     @Test
     public void ShouldBuyForMoreBonus() {
         int amount = 2_500;
         int expected = 500;
         int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
         System.out.println("Для получения бонуса Вам нужно докупить на сумму " + expected + " рублей");
     }
 
@@ -54,6 +62,8 @@ public class CashbackHackServiceTest {
         int amount = 0;
         int expected = 1_000;
         int actual = service.remain(amount);
+
+        assertEquals(actual, expected);
         System.out.println("Для получения бонуса Вам нужно докупить на сумму " + expected + " рублей");
     }
 
